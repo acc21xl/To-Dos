4e41fe3f9a15b1b2c67c67334c34e1fcbf579187
aca19lts <ltsargent1@sheffield.ac.uk>
2023-12-07 21:04:40 +0000

    workspace.xml

--- app/src/main/java/com/example/todo/screens/MoodScreen.kt
+++ app/src/main/java/com/example/todo/screens/MoodScreen.kt
@@ -1,68 +1,96 @@
 package com.example.todo.screens
 
-import android.Manifest
-import android.app.DatePickerDialog
-import android.content.Context
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import com.example.todo.enums.MoodScore
+import com.example.todo.viewmodels.TodosViewModel
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.material.icons.Icons
-import java.io.ByteArrayOutputStream
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.CameraAlt
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.autoSaver
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.content.ContextCompat
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavController
-import com.example.todo.data.TodoDatabase
-import com.example.todo.entities.DogEntity
-import com.example.todo.viewmodels.DogViewModel
-import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Date
-import java.util.Locale
-import com.example.todo.viewmodels.TodosViewModel
+import com.example.todo.enums.Priority
+import com.example.todo.TaskRow
+import com.example.todo.entities.TodoEntity
+import com.example.todo.viewmodels.MoodViewModel
 
-//@Composable
+@Composable
+fun MoodScreen(navController: NavController, viewModel: MoodViewModel, onBackClicked: () -> Unit) {
+    val dog by viewModel.dog.collectAsState()
+
+    Column{
+
+        Text(
+            text="Average Mood:",
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(8.dp)
+        )
+            // Ensure dog is not null before displaying details
+            if (dog != null) {
+                // Calculate average of last 5 scores
+                val avg: MoodScore = getLastFiveMoodsAvg()
+                Text(text = "${avg}")
+            }
+            // Display last 5 completed todos with scores
+            //still to do
+
+    }
+}
+
+private fun ColumnScope.Text(
+    text: String,
+    style: TextStyle,
+    modifier: Modifier,
+    function: () -> Unit
+) = Unit
+
+//Get average of last 5, return closest of the enum values
+fun getLastFiveMoodsAvg(): MoodScore {
+    val lastFive: Int
+    val avgMood: MoodScore = MoodScore.NEUTRAL
+    //val lastFive = getLastFiveMoods(): Int
+    //if lastFive >= 4.5:
+        //avgMood = EXCELLENT
+    //if 3.5 <= lastFive < 4.5:
+        //avgMood = HAPPY
+    //if 2.5 <= lastFive < 3.5:
+        //avgMood = NEUTRAL
+    //if 1.5 <= lastFive < 2.5:
+        //avgMood = SAD
+    //if lastFive < 1.5:
+        //avgMood = UPSET
+    return avgMood
+}
